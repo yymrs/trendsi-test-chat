d786506cd567ceefa04d5884f53d7ef9b978a57a
songyan <yan.song@trendsi.com>
2024-05-20 18:55:06 +0800

    切换接口test环境

--- src/api/index.cjs
+++ src/api/index.cjs
@@ -4,7 +4,7 @@ import httpsServer from "@/httpsServer/index.cjs";
  * @description 获取消息
  */
 export async function getChartDataApi(params = {}) {
-  const res = await httpsServer.post("https://d-app.trendsi.cc/api/v1/robot/chat", {
+  const res = await httpsServer.post("https://t-app.trendsi.cc/api/v1/robot/chat", {
     msgType: "2", // 消息类型1：text；2：pic；3：media；4：file；5emoji；
     device_id: "148189",
     channel: "3",
